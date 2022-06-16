3e97995d1e2e9b5ac128e78c7913ff41ece05b02
Gui <guilherme.rddev@gmail.com>
2022-06-16 10:40:41 -0300

    feat: configuracao para rodar s3 local

---
+++ src/main/java/br/com/gui/aws_app01/config/local/JmsConfigLocal.java
@@ -0,0 +1,46 @@
+package br.com.gui.aws_app01.config.local;
+
+import com.amazon.sqs.javamessaging.ProviderConfiguration;
+import com.amazon.sqs.javamessaging.SQSConnectionFactory;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.AmazonSQSClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.support.destination.DynamicDestinationResolver;
+import org.springframework.stereotype.Service;
+
+import javax.jms.Session;
+
+@Service
+@EnableJms
+@Profile("local")
+public class JmsConfigLocal {
+
+  @Value("${aws.region}")
+  private String awsRegion;
+
+  private SQSConnectionFactory sqsConnectionFactory;
+
+  @Bean
+  public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
+    sqsConnectionFactory = new SQSConnectionFactory(new ProviderConfiguration(),
+        AmazonSQSClient.builder().withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
+                "http://localhost:4566",
+                Regions.SA_EAST_1.getName()
+            ))
+            .withCredentials(new DefaultAWSCredentialsProviderChain())
+            .build());
+    final var factory = new DefaultJmsListenerContainerFactory();
+    factory.setConnectionFactory(sqsConnectionFactory);
+    factory.setDestinationResolver(new DynamicDestinationResolver());
+    factory.setConcurrency("2"); // Quantidade de thread por fila
+    factory.setSessionAcknowledgeMode(Session.CLIENT_ACKNOWLEDGE);
+
+    return factory;
+  }
+}
---
+++ src/main/java/br/com/gui/aws_app01/config/local/S3ConfigLocal.java
@@ -0,0 +1,114 @@
+package br.com.gui.aws_app01.config.local;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.BucketNotificationConfiguration;
+import com.amazonaws.services.s3.model.S3Event;
+import com.amazonaws.services.s3.model.TopicConfiguration;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.model.CreateTopicRequest;
+import com.amazonaws.services.sns.util.Topics;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClient;
+import com.amazonaws.services.sqs.model.CreateQueueRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("local")
+public class S3ConfigLocal {
+  private static final String BUCKET_NAME = "pcs-invoice";
+
+  private AmazonS3 amazonS3;
+
+  public S3ConfigLocal(AmazonS3 amazonS3) {
+    amazonS3 = getAmazonS3();
+
+    createBucket();
+
+    AmazonSNS snsClient = getAmazonSNS();
+
+    String s3InvoiceEvetsTopicArn = createTopic(snsClient);
+
+    AmazonSQS sqsClient = getAmazonSQS();
+
+    createQueue(snsClient, s3InvoiceEvetsTopicArn, sqsClient);
+
+    configureBucket(s3InvoiceEvetsTopicArn);
+  }
+
+  public AmazonS3 getAmazonS3() {
+    AWSCredentials credentials = new BasicAWSCredentials("test", "test");
+
+    this.amazonS3 = AmazonS3ClientBuilder.standard()
+        .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
+            "http://localhost:4566", Regions.SA_EAST_1.getName()))
+
+        .withCredentials(new AWSStaticCredentialsProvider(credentials))
+        .enablePathStyleAccess()
+        .build();
+    return this.amazonS3;
+  }
+
+  private void createBucket() {
+    this.amazonS3.createBucket(BUCKET_NAME);
+  }
+
+  @Bean
+  public AmazonS3 amazonS3Client() {
+    return this.amazonS3;
+  }
+
+  private AmazonSNS getAmazonSNS() {
+    return AmazonSNSClient.builder()
+        .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration
+            (
+                "http://localhost:4566",
+                Regions.SA_EAST_1.getName()
+            )
+        )
+        .withCredentials(new DefaultAWSCredentialsProviderChain())
+        .build();
+  }
+
+  private AmazonSQS getAmazonSQS() {
+    return AmazonSQSClient.builder()
+        .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration
+            (
+                "http://localhost:4566",
+                Regions.SA_EAST_1.getName()
+            )
+        )
+        .withCredentials(new DefaultAWSCredentialsProviderChain())
+        .build();
+  }
+
+  private String createTopic(AmazonSNS snsClient) {
+    final var createTopicRequest = new CreateTopicRequest("s3-invoice-events");
+    return snsClient.createTopic(createTopicRequest).getTopicArn();
+  }
+
+  private void createQueue(AmazonSNS snsClient, String s3InvoiceEvetsTopicArn, AmazonSQS sqsClient) {
+    final var productEventsQueueUrl =
+        sqsClient.createQueue(new CreateQueueRequest("s3-invoice-events")).getQueueUrl();
+
+    Topics.subscribeQueue(snsClient, sqsClient, s3InvoiceEvetsTopicArn, productEventsQueueUrl);
+  }
+
+  private void configureBucket(String s3InvoiceEvetsTopicArn) {
+    TopicConfiguration topicConfiguration = new TopicConfiguration();
+    topicConfiguration.setTopicARN(s3InvoiceEvetsTopicArn);
+    topicConfiguration.addEvent(S3Event.ObjectCreatedByPut);
+
+    amazonS3.setBucketNotificationConfiguration(BUCKET_NAME, new BucketNotificationConfiguration()
+        .addConfiguration("putObject", topicConfiguration));
+  }
+}
