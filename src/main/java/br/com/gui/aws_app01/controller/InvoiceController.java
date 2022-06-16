74aa41a8ee09f4fbea3e3ca02dadf20137c45c12
Gui <guilherme.rddev@gmail.com>
2022-06-16 10:12:52 -0300

    feat: pesquisar nossos invoice salvos

--- src/main/java/br/com/gui/aws_app01/controller/InvoiceController.java
+++ src/main/java/br/com/gui/aws_app01/controller/InvoiceController.java
@@ -1,6 +1,8 @@
 package br.com.gui.aws_app01.controller;
 
+import br.com.gui.aws_app01.model.Invoice;
 import br.com.gui.aws_app01.model.UrlResponse;
+import br.com.gui.aws_app01.repository.InvoiceRepository;
 import com.amazonaws.HttpMethod;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
@@ -8,8 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Duration;
@@ -24,11 +28,13 @@ public class InvoiceController {
   @Value("${aws.s3.bucket.invoice.name}")
   private String bucketName;
 
-  private AmazonS3 amazonS3;
+  private final AmazonS3 amazonS3;
+  private final InvoiceRepository invoiceRepository;
 
   @Autowired
-  public InvoiceController(AmazonS3 amazonS3) {
+  public InvoiceController(AmazonS3 amazonS3, InvoiceRepository invoiceRepository) {
     this.amazonS3 = amazonS3;
+    this.invoiceRepository = invoiceRepository;
   }
 
   @PostMapping
@@ -44,5 +50,15 @@ public class InvoiceController {
     urlResponse.setExpirationTime(expirationTime.getEpochSecond());
     urlResponse.setUrl(amazonS3.generatePresignedUrl(generatePresignedUrlRequest).toString());
     return new ResponseEntity<>(urlResponse, HttpStatus.OK);
+  }
+
+  @GetMapping()
+  public Iterable<Invoice> findAll() {
+    return invoiceRepository.findAll();
+  }
+
+  @GetMapping(path = "/bycustomername")
+  public Iterable<Invoice> findByCustomerName(@RequestParam String customerName) {
+    return invoiceRepository.findAllByCustomerName(customerName);
   }
 }
